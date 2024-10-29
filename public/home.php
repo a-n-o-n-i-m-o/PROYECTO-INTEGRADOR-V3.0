a601ae0adc11870ce5378fd5f971860574a4e351
a-n-o-n-i-m-o <alfredo051429@gmail.com>
2024-10-29 18:55:18 -0500

    segundo commit

---
+++ public/home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BCP - Créditos y Servicios</title>
+    <link href="https://fonts.googleapis.com/css2?family=KoHo&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <div class="header-content">
+            <div class="header-logo">
+                <h1><img src="https://www.bcp.com.bo/BilleteraMovil/Images/LogoBCP1.png" alt="BCP Logo"></h1>
+            </div>
+        </div>
+    </header>
+
+    <!-- Captaciones Section -->
+    <section class="section section-captaciones">
+        <h2 class="section-title">Captaciones</h2>
+        <div class="cards-container">
+            <div class="card">
+                <h3 class="card-title">Crédito Vehicular</h3>
+                <p class="card-text">Solicita tu crédito vehicular con las mejores tasas del mercado.</p>
+                <a href="../app/views/formulario_credito.php" class="interest-button">Me interesa</a>
+            </div>
+
+            <div class="card">
+                <h3 class="card-title">Crédito Universitario</h3>
+                <p class="card-text">Financia tus estudios con cuotas accesibles.</p>
+                <a href="index.php?route=credito_universitario" class="interest-button">Me interesa</a>
+            </div>
+            <div class="card">
+                <h3 class="card-title">Crédito Hipotecario</h3>
+                <p class="card-text">Compra la casa de tus sueños con facilidades de pago.</p>
+                <a href="index.php?route=credito_hipotecario" class="interest-button">Me interesa</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Colocaciones Section -->
+    <section class="section section-colocaciones">
+        <h2 class="section-title">Colocaciones</h2>
+        <div class="cards-container">
+            <div class="card">
+                <h3 class="card-title">Plazo Fijo</h3>
+                <p class="card-text">Ahorra con tasas preferenciales y asegura tu futuro.</p>
+                <a href="index.php?route=plazo_fijo" class="interest-button">Me interesa</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <div class="footer-content">
+            <p>© 2024 BCP | Todos los derechos reservados. Sede Central, Centenario 156, La Molina 15026, Lima, Perú. BANCO DE CREDITO DEL PERU S.A - RUC 20100047218</p>
+        </div>
+    </footer>
+</body>
+</html>
